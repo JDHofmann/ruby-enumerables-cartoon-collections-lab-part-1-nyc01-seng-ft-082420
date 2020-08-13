dff9f3e879fad2c7811c8c363304ee328635d55f
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-13 17:08:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index {
-    |item, index| p (index + 1) + ". " + item 
+    |item, index| p (index + 1).to_s + ". " + item 
   }
 end
